af1569791696bdcd7eeee1f4b86b56fd12a649fa
Ayoub <ayoub.slimanipro@gmail.com>
2023-01-13 01:15:18 +0100

    on peut désormais enlever la surbrillance sur le bouton + lorsqu'on appuie sur le bouton supprimer ou lorsque on clique sur un nouveau chiffre après avoir appuyer sur le +

--- addition.js
+++ addition.js
@@ -1,11 +1,34 @@
 let boutonPlus = document.querySelector(".addition");
 
+// Permet de savoir si un bouton à été cliqué
+let isButtonEraseClicked = false;
+let isButtonNombresClicked = false;
+
 let tableResultat = [];
 
+let eraseButton = document.querySelector(".eraseButton");
+
+
 function addition(){
+
     boutonPlus.addEventListener("click", function(){
         boutonPlus.style.backgroundColor = "lightgrey";
         tableResultat.push(maDivResultatCache[0]);
         console.log(maDivResultatCache.textContent);
     })
+
+
+    if(isButtonEraseClicked == true){
+        eraseButton.addEventListener('click', function(){
+            boutonPlus.style.backgroundColor = "#ff9f0a";
+            isButtonEraseClicked = false;
+        })
+    } else if(isButtonNombresClicked == true){
+        for(const nombre of nombres){
+            nombre.addEventListener('click', function(){
+                boutonPlus.style.backgroundColor = "#ff9f0a";
+                isButtonNombresClicked = false;
+            })
+            }
+    }
 }
--- writeNumber.js
+++ writeNumber.js
@@ -3,15 +3,18 @@ let monResultat = maDivResultat.innerHTML;
 
 let maDivResultatCache = maDivResultat;
 
-let nombres = document.querySelectorAll(".nombre");
+let nombres = document.querySelectorAll(".nombre"); 
+
 
 function writeNumber() {
 
-        if(maDivResultat.textContent == 0) {
+        /*if(maDivResultat.textContent == 0) {
                 //console.log("un click");
                 maDivResultat.innerHTML = "";
-        }
-        //console.log(maDivResultat.textContent);
+                }
+                //console.log(maDivResultat.textContent);
+        */
+        isButtonNombresClicked = true;
 
         for(const nombre of nombres){
                 nombre.addEventListener('click', function(){
@@ -20,13 +23,17 @@ function writeNumber() {
                         //console.log(tableResultat);
                 })
                 }
+                
+                console.log(monResultat);
                 maDivResultat.innerHTML += monResultat;        
+
         
                 if(maDivResultat.textContent.length >= 9) {
-                for(nombre of nombres){
-                        nombre.removeAttribute("onclick");
+                        for(const nombre of nombres){
+                                nombre.removeAttribute("onclick");
+                        }
                 }
-        }
+
         }
 
 
@@ -36,6 +43,8 @@ function eraseNumber() {
         for(nombre of nombres){
                 nombre.setAttribute("onclick", "writeNumber()");
         }
+        isButtonEraseClicked = true;
+        isButtonNombresClicked = false;
         return maDivResultat.textContent = "0";
 }
 
