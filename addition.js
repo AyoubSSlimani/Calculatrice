dfea322c08d8f304d276f4eb7089af48ae1db389
Ayoub <ayoub.slimanipro@gmail.com>
2023-02-18 13:05:58 +0100

    ajout de toute les opérations basic + ajout de nombre avec le clavier

--- addition.js
+++ addition.js
@@ -5,16 +5,22 @@ let buttonMultiply = document.querySelector(".multiplication");
 let buttonDivide = document.querySelector(".division");
 let buttonEgale = document.querySelector(".egale");
 let buttonColorOranges = document.querySelectorAll(".colorOrange");
+let buttonOperations = document.querySelectorAll(".operation");
+
 
 
 //Permet de savoir si un bouton à été cliqué
 let isButtonEraseClicked = false;
 let isButtonNombresClicked = false;
 let isButtonPlusClicked = false;
+let isButtonMultiplyClicked = false;
 let isButtonMinusClicked = false;
+let isButtonDivideClicked = false;
 let isButtonEgaleClicked = false;
-let nameOperation;
+let isButtonOperationClicked = false;
 let numberPop;
+let operationName;
+let countClick = 0;
 
 let eraseButton = document.querySelector(".eraseButton");
 
@@ -22,40 +28,94 @@ let eraseButton = document.querySelector(".eraseButton");
 function addNumber() {
     maDivResultatCache = maDivResultat;
     tableResultat.push(parseInt(maDivResultatCache.textContent));
-    if(isButtonEgaleClicked === true){
-        tableResultat.pop();
-    }
-    console.log(tableResultat);
-    maDivResultat.innerHTML = "";   
+    //maDivResultat.innerHTML = ""; 
     isButtonEgaleClicked = false;
 }
+addNumber();
 
 //Permet de séléctionner un bouton opérateur et d'appeler la fonction addNumber()
-function operation(buttonOperation){
-    buttonOperation.addEventListener("click", function(){
-        buttonOperation.style.backgroundColor = "lightgrey";
-        nameOperation = buttonOperation.className;
-        addNumber();
-        })
-    
+function operation(){
+    for (const operateur of buttonOperations){
+        operateur.addEventListener("click", function(){
+            addNumber();
+            switch(operateur.classList[2]) {
+                case "addition": 
+                    countClick = 0;
+                    countClick++;
+                    if(countClick === 1){
+                    whatTypeOfOperation();
+                    }
+                    operationName = "addition";
+                    isButtonDivideClicked = false;
+                    isButtonMinusClicked = false;
+                    isButtonMultiplyClicked = false;
+                    isButtonPlusClicked = true;
+                    if(isButtonPlusClicked){
+                        operateur.style.backgroundColor = "lightgrey";
+                    }
+                    addition();
+                    break;
+                case "soustraction": 
+                    countClick = 0;
+                    countClick++;
+                    if(countClick === 1){
+                        whatTypeOfOperation();
+                    }
+                    operationName = "soustraction";
+                    isButtonDivideClicked = false;
+                    isButtonPlusClicked = false;
+                    isButtonMultiplyClicked = false;
+                    isButtonMinusClicked = true;
+                    if(isButtonMinusClicked){
+                        operateur.style.backgroundColor = "lightgrey";
+                    }
+                    soustraction();
+                    console.log(countClick);
+                    break;
+                case "multiplication": 
+                    countClick = 0;
+                    countClick++;
+                    if(countClick === 1){
+                        whatTypeOfOperation();
+                    }
+                    operationName = "multiplication";
+                    isButtonPlusClicked = false;
+                    isButtonDivideClicked = false;
+                    isButtonMinusClicked = false;
+                    isButtonMultiplyClicked = true;
+                    if(tableResultat[0] == 0){
+                        tableResultat[0] = 1;
+                    }
+                    multiplication();
+                    break;   
+                case "division": 
+                    countClick = 0;
+                    countClick++;
+                    if(countClick === 1){
+                        whatTypeOfOperation();
+                    }
+                    operationName = "division";
+                    isButtonPlusClicked = false;
+                    isButtonMultiplyClicked = false;
+                    isButtonMinusClicked = false;
+                    isButtonDivideClicked = true;
+                    division();
+                    break; 
+                default:
+                    console.log("je ne comprends pas l'opération choisie");
+            }
+            })
+    }
         buttonOrange();
 }
-operation(buttonPlus);
-operation(buttonMinus);
-operation(buttonMultiply);
-operation(buttonDivide);
+operation();
+//operation(buttonPlus);
+//operation(buttonMinus);
+//operation(buttonMultiply);
+//operation(buttonDivide);
 
-
-
-//Permet de donner le résultat suivant l'opération qu'on a effectué
-function egale() {
-    buttonEgale.addEventListener("click", function(){
-        buttonEgale.style.backgroundColor = "lightgrey";
-        tableResultat.push(parseInt(maDivResultatCache.textContent));
-        console.log(tableResultat);
+function addition(){
         maDivResultatCache = 0;
-        if(nameOperation.includes("addition")){
-            isButtonPlusClicked = true;
             for(i = 0; i < tableResultat.length; i++){
                 if(lastResultat != 0){
                     maDivResultatCache = lastResultat;
@@ -63,36 +123,71 @@ function egale() {
                 } else {
                     maDivResultatCache += tableResultat[i];
                 }
+                maDivResultat.innerHTML = maDivResultatCache;
+                lastResultat = maDivResultatCache;
             }
-        }else if(nameOperation.includes("soustraction")){
-            isButtonMinusClicked = true;
-            for(i = 0; i < tableResultat.length; i++){
-                if(i !== tableResultat.length-1){
-                    maDivResultatCache = tableResultat[i] - tableResultat[i+1];
-                }
-            }
-        }else if(nameOperation.includes("multiplication")){
-            maDivResultatCache = 1;
+            tableResultat = [];
+}
+
+
+function multiplication(){
+        maDivResultatCache = 1;
             for(i = 0; i < tableResultat.length; i++){
                 if(lastResultat != 0){
                     maDivResultatCache = lastResultat;
                     maDivResultatCache *= tableResultat[i];
-                }else {
+                } else {
                     maDivResultatCache *= tableResultat[i];
                 }
+                maDivResultat.innerHTML = maDivResultatCache;
+                lastResultat = maDivResultatCache;
             }
-        }else if(nameOperation.includes("division")){
-            //maDivResultatCache = 0;
+            tableResultat = [];
+}
+
+function soustraction(){
+    maDivResultatCache = 0;
             for(i = 0; i < tableResultat.length; i++){
-                if(i !== tableResultat.length-1){
-                    maDivResultatCache += tableResultat[i] / tableResultat[i+1];
-                    }
+                if(lastResultat != 0){
+                    maDivResultatCache = lastResultat;
+                    maDivResultatCache -= tableResultat[i];
+                } else if(tableResultat[0] == 0) {
+                    maDivResultatCache += tableResultat[i];
+                } else {
+                    maDivResultatCache += tableResultat[i];
+                }
+                maDivResultat.innerHTML = maDivResultatCache;
+                lastResultat = maDivResultatCache;
             }
+            tableResultat = [];
+}
+
+function division(){
+    maDivResultatCache = 0;
+        for(i = 0; i < tableResultat.length; i++){
+            if(lastResultat != 0){
+                maDivResultatCache = lastResultat;
+                maDivResultatCache /= tableResultat[i];
+            } else if(tableResultat[0] == 0){
+                maDivResultatCache += tableResultat[i];
+            }
+            else {
+                maDivResultatCache += tableResultat[i];
+            }
+            maDivResultat.innerHTML = maDivResultatCache;
+            lastResultat = maDivResultatCache;
         }
-        maDivResultat.innerHTML = maDivResultatCache;
-        lastResultat = maDivResultatCache;
-        console.log(lastResultat);
-        console.log(maDivResultatCache);
+        tableResultat = [];
+}
+
+
+//Permet de donner le résultat suivant l'opération qu'on a effectué
+function egale() {
+    buttonEgale.addEventListener("click", function(){
+        buttonEgale.style.backgroundColor = "lightgrey";
+        maDivResultatCache = maDivResultat;
+        tableResultat.push(parseInt(maDivResultatCache.textContent));
+        whatTypeOfOperation();
         isButtonEgaleClicked = true;
     })
 
@@ -119,4 +214,23 @@ function buttonOrange() {
         }
 }
 
-
+//fonction qui permet de vérifier l'opération qui a été utilisé
+function whatTypeOfOperation(){
+    switch(operationName){
+        case "addition":
+            addition();
+            break;
+        case "soustraction":
+            soustraction();
+            break;
+        case "multiplication":
+            multiplication();
+            break;
+        case "division":
+            division();
+            break;
+        default:
+            console.log("opération inconnue");
+            break;
+    }
+}
--- writeNumber.js
+++ writeNumber.js
@@ -15,11 +15,19 @@ let limitNumber = false;
 //Fonction qui permet de détecter sur quel nombre on click et qui affiche ce nombre 
 function writeNumber(){
         for(const nombre of nombres){
-
+        
         nombre.addEventListener("click", function() {
                 if(limitNumber === false){
                         if(maDivResultat.textContent == 0) {
                                 maDivResultat.innerHTML = "";
+                        } else if(isButtonPlusClicked || isButtonMultiplyClicked || isButtonMinusClicked || isButtonDivideClicked){
+                                maDivResultat.innerHTML = "";
+                                
+                                isButtonPlusClicked = false;
+                                isButtonMultiplyClicked = false;
+                                isButtonMinusClicked = false;
+                                isButtonDivideClicked = false;
+                                
                         } else if(maDivResultat.textContent.length >= 8){
                                 limitNumber = true;
                         }        
@@ -31,12 +39,39 @@ function writeNumber(){
                         }
                 }
                 )
-                
         }
 }
 
 writeNumber();
 
+//Fonction qui permet d'ajouter des nombres lorsque l'on appuye sur une touche du clavier
+function writeKeyBoard(){
+        document.addEventListener("keydown", (event) => {
+                const nomTouche = event.key;
+                if(!isNaN(nomTouche)){
+                        if(limitNumber === false){
+                                if(maDivResultat.textContent == 0) {
+                                        maDivResultat.innerHTML = "";
+                                } else if(isButtonPlusClicked || isButtonMultiplyClicked || isButtonMinusClicked || isButtonDivideClicked){
+                                        maDivResultat.innerHTML = "";
+                                        
+                                        isButtonPlusClicked = false;
+                                        isButtonMultiplyClicked = false;
+                                        isButtonMinusClicked = false;
+                                        isButtonDivideClicked = false;
+                                        
+                                } else if(maDivResultat.textContent.length >= 8){
+                                        limitNumber = true;
+                                }        
+                                                
+                                isButtonNombresClicked = true;
+                                        
+                                maDivResultat.innerHTML += nomTouche; 
+                                }
+                }
+        })
+}
+writeKeyBoard();
 
 //Fonction qui permet d'effacer l'affichage et de le remettre à zéro
 function eraseNumber() {
@@ -44,12 +79,14 @@ function eraseNumber() {
                 isButtonEraseClicked = true;
                 isButtonNombresClicked = false;
                 isButtonPlusClicked = false;
-                isButtonPlusClicked = false
+                isButtonMinusClicked = false;
+                isButtonMultiplyClicked = false;
+                isButtonDivideClicked = false;
                 isButtonEgaleClicked = false;
                 limitNumber = false;
                 tableResultat = [];
                 lastResultat = 0;
-                return maDivResultat.textContent = "0";
+                return maDivResultat.innerHTML = "0";
         })
 }
 
